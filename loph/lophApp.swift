8aa38f6d1dfc9c33ed1bfe62c510d00914465ce4
Ariel Thunstrom <arielthunstrom05@gmail.com>
2023-04-05 08:42:41 -0500

    Initial Commit

---
+++ loph/lophApp.swift
@@ -0,0 +1,17 @@
+//
+//  lophApp.swift
+//  loph
+//
+//  Created by 90309356 on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct lophApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
