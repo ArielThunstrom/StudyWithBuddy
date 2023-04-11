3012293e2d863cca420fcd55d79c7f359c058a99
Ariel Thunstrom <arielthunstrom05@gmail.com>
2023-04-11 08:59:54 -0500

    removing stuff

--- loph/clothingView.swift
+++ loph/clothingView.swift
@@ -1,85 +1,37 @@
 //
-//  FilterView.swift
-//  LostFound
+//  clothingView.swift
+//  loph
 //
-//  Created by 90305906 on 3/30/22.
+//  Created by 90309356 on 4/11/23.
 //
-/*
+
 import SwiftUI
 
 struct clothingView: View {
-    
-    @Environment(\.presentationMode) var presentationMode
-    
-    @EnvironmentObject var model: ItemModel
-    
-    
-    
     var body: some View {
-        
-        NavigationView {
-            VStack {
-                Form {
-                    Section {
-                        Picker("Sort By", selection: $model.enteredSort) {
-                            ForEach(model.sortTypes, id: \.self) {
-                                Text($0)
-                            }
-                        }
-                    }
-                    
-                    Section {
-                        Toggle("T-Shirt", isOn: $model.showTShirt)
-                        Toggle("Sweatshirt", isOn: $model.showSweatshirt)
-                        Toggle("Shorts", isOn: $model.showShorts)
-                        Toggle("Pants", isOn: $model.showPants)
-                        Toggle("Hat", isOn: $model.showHat)
-                        Toggle("Water Bottle", isOn: $model.showWaterBottle)
-                        Toggle("Jewelry", isOn: $model.showJewelry)
-                        Toggle("Other", isOn: $model.showOther)
-                    }
+        ZStack{
+            //put a background image here
+            ScrollView {
+                LazyVGrid(columns: [
+                    GridItem(.flexible()),
+                    GridItem(.flexible()),
+                    GridItem(.flexible())
+                ], spacing: 3){
+                    //put clothing items here
                 }
-                Button {
-                    model.sortData(sortBy: model.enteredSort)
-                    presentationMode.wrappedValue.dismiss()
-                    
-                } label: {
-                    Text("Show Results")
-                }.frame(width: 250, height: 25)
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color.blue)
-                    .cornerRadius(20)
-                    
-                
-            }.toolbar {
-                ToolbarItemGroup {
-                    Button {
-                        model.filterReset()
-                    } label: {
-                        Text("Reset")
-                    }
-                }
-            }
-                .navigationTitle("Add Item")
-                .navigationBarTitleDisplayMode(.inline)
+            }.coordinateSpace(name: "scroll")
+                /*
+                .safeAreaInsert(edge: .top, content: {
+                    Color.clear
+                        .frame(height: 3)//idk what the hieght should be
+                })
+                 */
         }
     }
-    
-//    var filteredSearchResult: [Item] {
-//        return model.searchResults.filter { _ in if showTShirt { model.items.type.contains("t-shirt") } || if showSweatshirt { $0.type.contains("sweatshirt") } }
-//    }
-    
-    func filteredSearchResults() {
-        
-    }
-    
 }
 
-struct FilterView_Previews: PreviewProvider {
+struct clothingView_Previews: PreviewProvider {
     static var previews: some View {
         clothingView()
     }
 }
- */
-  
