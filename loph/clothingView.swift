3176aacef3b607060505cc45ae54ad1a3b4264b5
Ariel Thunstrom <arielthunstrom05@gmail.com>
2023-04-12 08:47:19 -0500

    closet UI

--- loph/clothingView.swift
+++ loph/clothingView.swift
@@ -7,9 +7,18 @@
 
 import SwiftUI
 
+let green = Color(red: 0.808, green: 0.847, blue: 0.78)
+
+let data = (1...100).map { "Item \($0)" }
+
+let clothing = ["shoes", "collar", "high heels", "a", "b", "c" ]
+//@State private var clothing = ["shoes", "collar", "high heels", "a", "b", "c" ]
+
 struct clothingView: View {
     var body: some View {
         ZStack{
+            green
+                .ignoresSafeArea()
             //put a background image here
             ScrollView {
                 LazyVGrid(columns: [
@@ -17,7 +26,9 @@ struct clothingView: View {
                     GridItem(.flexible()),
                     GridItem(.flexible())
                 ], spacing: 3){
-                    //put clothing items here
+                    ForEach(data, id: \.self) { item in
+                        Text(item)
+                    }
                 }
             }.coordinateSpace(name: "scroll")
                 /*
@@ -28,6 +39,25 @@ struct clothingView: View {
                  */
         }
     }
+    var items: some View {
+        ForEach(clothing, id: \.self){ item in
+            
+            if let url = item.image, let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
+                Image(uiImage: image)
+                    .resizable()
+                    .frame(width: UIScreen.main.bounds.width/3, height: UIScreen.main.bounds.width/3)
+                    .onTapGesture {
+                        
+                    }
+                    .background(Image(systemName: "photo")
+                        .foregroundColor(.white)
+                        .frame(width: UIScreen.main.bounds.width/3, height: UIScreen.main.bounds.width/3)
+                        .background(Color.gray))
+                    .foregroundColor(.white)
+            }
+        }
+    }
+
 }
 
 struct clothingView_Previews: PreviewProvider {
